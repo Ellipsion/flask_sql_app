16dd8e4a43e37164adf63036ecd10bb929ec9f81
Ashish Kumawat <ashishskyblue@gmail.com>
2023-06-23 22:10:44 +0530

    added db

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 
+# app
 app = Flask(__name__)
 
+# db config
+db_uri = os.getenv("MYSQL_URL")
+db_config_key = "SQLALCHEMY_DATABASE_URI"
+app.config[db_config_key] = db_uri
 
-@app.route('/')
+# initialize database
+db = SQLAlchemy(app)
+
+# Create db model
+
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    variable = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+
+
+@app.route("/", methods=["GET"])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == "GET":
+        id = 1  # get the variable by id=1
+        test = Test.query.get(id)
+        return f"{test.variable} {test.date_created}"
+    else:
+        return "This is a GET only API."
 
 
 if __name__ == '__main__':
